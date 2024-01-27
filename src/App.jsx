42ab7266b082d42e3b74ae57cad8af60b8a0b542
abdullah-alashek <e.abdullah.alashek@gmail.com>
2024-01-27 12:06:46 +0300

    add8

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@
 import './App.css'
 import React from 'react'
 // import Home from './components/home/Home'
-// import NavBar from './components/navbar/NavBar'
+import NavBar from './components/navbar/NavBar'
 // import About from './components/about/About'
 // import Portfolio from './components/portfolio/Portfolio'
 // import Contact from './components/contact/contact'
-// import Aos from 'aos'
-// import 'aos/dist/aos.css'
-// import { useEffect, useRef } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect, useRef } from 'react'
 import Footer from './components/footer/Footer'
 // import scrollElement from 'react-scroll/modules/mixins/scroll-element'
 
 function App() {
-  // useEffect(()=>{
-  //   Aos.init()
-  // },[])
+  useEffect(()=>{
+    Aos.init()
+  },[])
   return (
     <>
       {/* < NavBar /> */}
