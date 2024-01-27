0d3e90b0917ce45af0d77e8a365658aab6709992
abdullah-alashek <e.abdullah.alashek@gmail.com>
2024-01-27 11:50:09 +0300

    add4

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import Particles from 'react-tsparticles'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react'
-import Home from './components/home/Home'
-import NavBar from './components/navbar/NavBar'
-import About from './components/about/About'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/contact'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect, useRef } from 'react'
-import Footer from './components/footer/Footer'
-import scrollElement from 'react-scroll/modules/mixins/scroll-element'
 
 function App() {
-  useEffect(()=>{
-    Aos.init()
-  },[])
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      < NavBar />
-      < Footer/>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
