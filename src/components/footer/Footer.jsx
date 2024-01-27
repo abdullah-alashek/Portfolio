3b14e58f5d5613c4c2daf31120d16d8cf6fa0ad8
abdullah-alashek <e.abdullah.alashek@gmail.com>
2024-01-27 11:35:45 +0300

    new

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer-page'>
+        <footer>
+            <p>Copyright © Abdullah Alashek</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
