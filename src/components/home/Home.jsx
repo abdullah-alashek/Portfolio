c87fc66ec73eab282f8744430c6378d39c62470b
abdullah-alashek <e.abdullah.alashek@gmail.com>
2024-01-27 11:46:00 +0300

    add2

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,62 @@
+import React  from 'react'
+import './Home.css'
+import BackParticles from '../../assets/const/BackParticles'
+import myPic from './../../assets/images/my.png'
+import { TypeAnimation } from 'react-type-animation'
+import { Link } from 'react-scroll'
+const Home = ({id}) => {
+  // const handleClickScroll = () => {
+  //   const element = document.getElementById('section-1');
+  //   if (element) {
+  //     // 👇 Will scroll smoothly to the top of the next section
+  //     element.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // };
+  return (
+    <>
+    <div className="homepage" id={id}>
+      < BackParticles />
+      <div className='container'>
+        <div className='content'>
+          <div className="image">
+            <img src={myPic} alt="" />
+          </div>
+          <div className="title">
+          <h1>Hi, I am <span>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Web Developer',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Abdullah-Alashek',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{  display: 'inline-block' }}
+              repeat={Infinity}
+            />
+          </span></h1>
+          </div>
+        
+        <p>Welcome! I’m Abdullah Al-Ashek, a passionate Computer Engineer specializing in website development. I strive to create digital platforms that are not only visually appealing but also user-friendly and functional. Let’s transform your ideas into reality!</p>
+              <div className="btn">
+                <Link
+                      activeClass="active"
+                      to="contact"
+                      spy={true}
+                      smooth={true}
+                      duration={500}>
+                        <a className="btn-cont"><button>Contact with me</button></a>
+                    </Link>
+                <a className="btn-down"target="_blank" href="https://drive.google.com/file/d/1pDprEISBvdylYqJvYZFpIH8Zdt9ugVdN/view?usp=sharing"><button>Download CV</button></a>
+              </div>
+        </div>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./NavBar.css"
+import Home from '../home/Home'
+import About from '../about/About'
+import Portfolio from '../portfolio/Portfolio'
+import { Link } from 'react-scroll'
+import Contact from '../contact/contact'
+const NavBar = () => {
+  return (
+    <>
+    <div className="navbar">
+        <div className="logo">
+            <span className="half-color" attribute="A">A</span>
+            <span className="half-color" attribute="b">b</span>
+            <span className="half-color" attribute="d">d</span>
+            <span className="half-color" attribute="u">u</span>
+            <span className="half-color" attribute="l">l</span>
+            <span className="half-color" attribute="l">l</span>
+            <span className="half-color" attribute="a">a</span>
+            <span className="half-color" attribute="h">h</span>
+            <span className="half-color" attribute="A">A</span>
+            <span className="half-color" attribute="l">l</span>
+            <span className="half-color" attribute="a">a</span>
+            <span className="half-color" attribute="s">s</span>
+            <span className="half-color" attribute="h">h</span>
+            <span className="half-color" attribute="e">e</span>
+            <span className="half-color" attribute="k">k</span>
+        </div>
+
+
+        <div className="menu">
+            <ul>
+                <li><Link
+                      activeClass="active"
+                      to="home"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                    >
+                      Home
+                    </Link></li>
+                <li><Link
+                      activeClass="active"
+                      to="about"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                    >
+                      About
+                    </Link></li>
+                <li><Link
+                      activeClass="active"
+                      to="portfolio"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                    >
+                      Portfolio
+                    </Link></li>
+                <li><Link
+                      activeClass="active"
+                      to="contact"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                    >
+                      Contact
+                    </Link></li>
+            </ul>
+        </div>
+    </div>
+    < Home id='home' />
+      < About id='about' />
+      < Portfolio id='portfolio' />
+      <Contact id='contact'/>
+
+    </>
+  )
+}
+
+export default NavBar
